f3bb60fdc695a5a5b320d0b465f8e4e6b389f7ae
qiuye-dev <2635053757@qq.com>
2026-02-03 14:14:29 +0800

    初始化项目

---
+++ src/main/java/com/qiuye/yeaiagent/YeAiAgentApplication.java
@@ -0,0 +1,13 @@
+package com.qiuye.yeaiagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YeAiAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YeAiAgentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/qiuye/yeaiagent/YeAiAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qiuye.yeaiagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YeAiAgentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
